0177f69757a3f8a50f56b90a90057c366808ed90
Roberta Vieira de Melo <roberta.vieirademelo@yahoo.com.br>
2022-07-05 22:04:46 -0300

    Aula - Renderização de listas 1

--- modulo-3/estados-insta4/insta4/src/App.js
+++ modulo-3/estados-insta4/insta4/src/App.js
@@ -9,29 +9,43 @@ const MainContainer = styled.div`
   align-items: center;
 `
 function App() {
-return(
-  <MainContainer>
-           <Post
-            nomeUsuario={'paulinha'}
-            fotoUsuario={'https://picsum.photos/50/50'}
-            fotoPost={'https://picsum.photos/200/150'}
-          />
 
-          <Post
-            nomeUsuario={'luizinho'}
-            fotoUsuario={'https://i.ytimg.com/vi/-AMbVaYtwcE/maxresdefault.jpg'}
-            fotoPost={'https://res.cloudinary.com/practicaldev/image/fetch/s--jIjY95PE--/c_imagga_scale,f_auto,fl_progressive,h_900,q_auto,w_1600/https://images.unsplash.com/photo-1529675641475-78780f1fd4b0%3Fixlib%3Drb-1.2.1%26ixid%3DeyJhcHBfaWQiOjEyMDd9%26auto%3Dformat%26fit%3Dcrop%26w%3D1350%26q%3D80'}
-          />
+  const arrayDePostagem = [
+    {
+      nomeUsuario: "Paula",
+      fotoUsuario: "https://picsum.photos/50/53",
+      fotoPost: "https://picsum.photos/200/153"
+    },
+    {
+      nomeUsuario: "Luiz",
+      fotoUsuario: "https://picsum.photos/50/54",
+      fotoPost: "https://picsum.photos/200/154"
+    },
+    {
+      nomeUsuario: "Carlos",
+      fotoUsuario: "https://picsum.photos/50/55",
+      fotoPost: "https://picsum.photos/200/155"
+    }
+  ]
 
-          <Post
-            nomeUsuario={'carlinhos'}
-            fotoUsuario={'https://forum.macmagazine.com.br/uploads/monthly_2021_10/IMG_4234.thumb.jpg.ff57b0e831aeed1fb8ea4e66540c3b08.jpg'}
-            fotoPost={'https://rc.am.br/galeria/arquivos/noticias/62745/big/6f158ab8f3be06619f584e36653dc5ca.jpg'}
-          />
-        </MainContainer>
-)
+  const pessoas = arrayDePostagem.map((pessoa, index) => {
+    return (
+      <Post key={index}
+        nomeUsuario = {pessoa.nomeUsuario}
+        fotoUsuario = {pessoa.fotoUsuario}
+        fotoPost = {pessoa.fotoPost}
+      />
+    )
+  })
 
+  console.log(pessoas)
+  
+
+  return (
+    <MainContainer>
+      {pessoas}
+    </MainContainer>
+  )
 }
-
 
 export default App;
--- modulo-3/estados-insta4/insta4/src/components/IconeComContador/IconeComContador.js
+++ modulo-3/estados-insta4/insta4/src/components/IconeComContador/IconeComContador.js
@@ -10,7 +10,7 @@ const IconImage = styled.img`
 
 export function IconeComContador(props) {
 	return <IconContainer>
-		<IconImage alt={'Icone'} src={props.icone} onClick={props.onClickIcone}/>
+		<IconImage alt={'Icone'} src={props.icone} onClick={props.onClickIcone} />
 		<p>{props.valorContador}</p>
 	</IconContainer>
 }
