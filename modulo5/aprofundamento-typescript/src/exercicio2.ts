b6838076897d5203cd1b77f5a9928f0e69e56280
Roberta Vieira de Melo <roberta.vieirademelo@yahoo.com.br>
2022-10-18 19:50:58 -0300

    [exercicio2]

--- modulo5/aprofundamento-typescript/src/exercicio2.ts
+++ modulo5/aprofundamento-typescript/src/exercicio2.ts
@@ -1,1 +1,33 @@
+type amostra = {
+    numeros: number[],
+    obterEstatisticas:  (numeros: number[]) => {}
+}
 
+const amostraDeIdades: amostra = {
+    numeros: [21, 18, 65, 44, 15, 18],
+    obterEstatisticas: (numeros: number[]) => {
+        const numerosOrdenados: number[] = numeros.sort(
+           (a: number, b: number) => a - b
+           
+           )
+       
+           let soma: number = 0
+       
+           for(let num of numeros) {
+               soma += num
+       
+           }
+       
+           const estatisticas = {
+               maior: numerosOrdenados[numeros.length -1],
+               menor: numerosOrdenados[0],
+               media: soma / numeros.length
+           }
+       
+           return estatisticas
+       
+       } 
+    
+}
+
+console.table(amostraDeIdades.obterEstatisticas(amostraDeIdades.numeros))
