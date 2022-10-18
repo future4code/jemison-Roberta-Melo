92ecc5c1be66092fd5e4ddde6535bbdd6b366dd0
Roberta Vieira de Melo <roberta.vieirademelo@yahoo.com.br>
2022-10-18 19:55:11 -0300

    [exercicio3]

---
+++ modulo5/aprofundamento-typescript/src/exercicio3.ts
@@ -0,0 +1,37 @@
+type Postagens = {
+    autor: String,
+    texto: String
+}
+
+const posts = [
+    {
+      autor: "Alvo Dumbledore",
+      texto: "Não vale a pena viver sonhando e se esquecer de viver"
+    },
+    {
+      autor: "Severo Snape",
+      texto: "Menos 10 pontos para Grifinória!"
+    },
+    {
+      autor: "Hermione Granger",
+      texto: "É levi-ô-sa, não levio-sá!"
+    },
+    {
+      autor: "Dobby",
+      texto: "Dobby é um elfo livre!"
+    },
+    {
+      autor: "Lord Voldemort",
+      texto: "Avada Kedavra!"
+    }
+  ]
+
+    //b)
+    function buscarPostsPorAutor(posts: Postagens[], autorInformado: string) {
+        return posts.filter(
+          (post) => {
+            return post.autor === autorInformado
+          }
+        )
+      }
+      console.log(buscarPostsPorAutor(posts, "Lord Voldemort"))
