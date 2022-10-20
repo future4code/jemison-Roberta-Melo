35141e13b9c743e933187fda599c5e870b9df53c
Roberta Vieira de Melo <roberta.vieirademelo@yahoo.com.br>
2022-10-20 18:37:47 -0300

    exercicio2

---
+++ modulo5/lista-typescript/src/exercicio2.ts
@@ -0,0 +1,11 @@
+const possibilidades = (): void =>{
+    const palavras: string = "";
+    const numeros: number = 0
+    const booleans: boolean = false
+
+    console.log(typeof(palavras) === "string")
+    console.log(typeof(numeros) === "boolean")
+    console.log(typeof(booleans) === "boolean")
+
+}
+possibilidades()
