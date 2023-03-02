a36cf09defe86318bf47ca636feed5d4fdab6c36
Roberta Vieira de Melo <roberta.vieirademelo@yahoo.com.br>
2023-03-02 18:02:41 -0300

    Aula - Testes unitários e Mocks

---
+++ modulo6/testes-unitarios-mocks/tests/validateCharacter.test.ts
@@ -0,0 +1,36 @@
+import { validateCharacter } from "../src/validateCharacter";
+
+describe("Test expected results of validateCharacter", () => {
+  test("Should return false for empty name", () => {
+    const result = validateCharacter({
+      name: "",
+      life: 1500,
+      strength: 300,
+      defense: 500,
+    });
+
+    expect(result).toBe(false);
+  });
+});
+
+test("Should return false for life 0", () => {
+  const result = validateCharacter({
+    name: "Scorpion",
+    life: 0,
+    strength: 300,
+    defense: 500,
+  });
+
+  expect(result).toBe(false);
+});
+
+test("Should return true for all valid inputs", () => {
+  const result = validateCharacter({
+    name: "Scorpion",
+    life: 1500,
+    strength: 300,
+    defense: 500,
+  });
+
+  expect(result).toBe(true);
+});
